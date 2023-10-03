85afd748e6a078855f64e669a315a061cd94fb73
shady khalifa <89072052+shady78@users.noreply.github.com>
2023-10-03 17:46:35 +0300

    Add project files.

---
+++ P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        [BindNever]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="ErrorMissingName")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Details { get; set; }
+
+        public string Stock { get; set; }
+
+        [Required(ErrorMessage ="ErrorMissingPrice")]
+        public string Price { get; set; }
+    }
+}
